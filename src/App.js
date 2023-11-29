4fb89dfc313b51dabc0fa610018d58c1dec997d1
David John <david.john@mail.utoronto.ca>
2023-11-29 15:24:26 -0500

    Set up App.js with Home.js and Footer.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from './Components/Home';
+import Footer from './Components/Footer';
 
 function App() {
+  let app = {
+    backgroundImage: "#eeeeee",
+    textAlign: "center",
+    fontFamily: `"Work Sans", sans-serif`,
+    margin: "auto",
+    maxWidth: "100%"
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className={app} fluid>
+      <Home />
+      
+      <br />
+      <br />
+      <Footer />
+    </Container>
   );
 }
 
