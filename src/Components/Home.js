cbebb656b701acfda92c0cbf3b2f5d6c5588e3dd
David John <david.john@mail.utoronto.ca>
2023-12-04 13:47:01 -0500

    Started laying out the divs for the password generation section

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,6 @@
-import Container from 'react-bootstrap/Container';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container, Stack, Card } from 'react-bootstrap';
+
 
 function Home() {
   let page = {
@@ -9,12 +10,27 @@ function Home() {
     margin: "auto",
     maxWidth: "100%"
   }
+
+  // First div in the stack will be the line where the password that is generated will show up as well as a refresh button to redo the password based on the entered parameters.
+  function PasswordLine() {
+    return (
+      <>
+        <Card>
+          <Card.Body>Placeholder text for generated password.</Card.Body>
+        </Card>
+      </>
+    );
+  }
+
   return (
     <Container style={page} fluid>
-
+      <Stack gap={3}>
+        <PasswordLine />
+        <div className="p-2">Second item</div>
+        <div className="p-2">Third item</div>
+      </Stack>
       <br />
       <br />
-
     </Container>
   );
 }
