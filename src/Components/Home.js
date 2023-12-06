3c17a24860701dacac358a346b87872c3a71adc3
David John <david.john@mail.utoronto.ca>
2023-12-06 13:40:52 -0500

    Implemented ProgressBar to PasswordLine (other work in progress, not implemented)

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,5 +1,5 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Container, Stack, Card } from 'react-bootstrap';
+import { Container, Stack, Card, Row, Col, Form, ProgressBar } from 'react-bootstrap';
 
 
 function Home() {
@@ -8,19 +8,46 @@ function Home() {
     textAlign: "center",
     fontFamily: `"Work Sans", sans-serif`,
     margin: "auto",
-    maxWidth: "100%"
+    maxWidth: "70%"
   }
 
   // First div in the stack will be the line where the password that is generated will show up as well as a refresh button to redo the password based on the entered parameters.
   function PasswordLine() {
+    // Includes progress bar to judge whether the password is decent or not.
+    // The 'now' and 'variant' and 'label' will be based on a function that is checks whether the generated password is actually strong enough.
+
+    let pwLine = {
+      fontFamily: `"Lucida Console","Courier New",Monaco,"Nimbus Mono L",monospace`,
+    }
     return (
       <>
         <Card>
-          <Card.Body>Placeholder text for generated password.</Card.Body>
+          <Card.Body style={pwLine}>
+            <Card.Title>Placeholder text for generated password.</Card.Title>
+          </Card.Body>
+          <ProgressBar now={60} variant="warning" label={'Not strong enough'}/>
         </Card>
       </>
     );
   }
+
+  // Second item in stack will include input text field which captures the users intended memorable phrase that will be leeted. The memorable phrase should be matched against the pwnedpassword api: https://haveibeenpwned.com/Passwords
+
+  function PasswordSelectors() {
+    // First col: slider for password length
+    // Second col: Specifiers for generated passwords
+    // Third col: Checkbox list for whether the user wants uppercase, lowercase, numbers and special characters
+    return (
+      <Card>
+        <Row>
+          <Col>
+
+          </Col>
+        </Row>
+      </Card>
+    );
+  }
+
 
   return (
     <Container style={page} fluid>
