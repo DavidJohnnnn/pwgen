7ca5f9b4193e4e86dddbc8d1cc14961f22da7ff1
David John <david.john@mail.utoronto.ca>
2023-12-12 14:37:56 -0500

    Started building third row where Form.Control for password input will be.

---
+++ src/Components/PWRow3.js
@@ -0,0 +1,13 @@
+import {Card, Form, Row, Col } from 'react-bootstrap';
+import { useState } from 'react';
+
+export default function PasswordInput () {
+  
+  return (
+    <Card>
+      <Row>
+        
+      </Row>
+    </Card>
+  );
+}
